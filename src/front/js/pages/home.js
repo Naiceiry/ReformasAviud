b1586bc6712180506013bce1ae3346ab8d542078
Naiceiry <naiceirymp@gmail.com>
2022-04-29 18:10:15 +0000

    video

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,13 +1,20 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import ReactPlayer from "react-player";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
     <div className="text-center mt-5">
-      ç
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=rnwlWn603g4"
+        className="react-player"
+        playing
+        width="100%"
+        height="100%"
+      />
     </div>
   );
 };
