5c2d71907a22796b9f7c18c28ee3ace6377e1979
Naiceiry <naiceirymp@gmail.com>
2022-04-29 18:00:48 +0000

    iniciar home

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-
+import gmail from "../../img/gmail.jpg";
+import wsapp from "../../img/wsapp.jpg";
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center">
+    <a className="navbar-brand" href="#">
+      <img className="img-footer" src={gmail} />
+    </a>
+    <a className="navbar-brand" href="#">
+      <img className="img-footer" src={wsapp} />
+    </a>
+  </footer>
 );
