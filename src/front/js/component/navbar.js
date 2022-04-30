10a7546ff17f5fc8bb0821a5b6affff10e2d28a4
Naiceiry <naiceirymp@gmail.com>
2022-04-30 14:11:04 +0000

    stile

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,75 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../img/LOGO-JPEG.jpg";
+import logo from "../../img/circularlogo.jpg";
 
 export const Navbar = () => {
   return (
     <nav className="navbar_blue navbar navbar-expand-lg navbar-light ">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <img className="logo-navb" src={logo} />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="ul-nav collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </a>
-              <ul
-                className="dropdown-menu"
-                aria-labelledby="navbarDropdownMenuLink"
-              >
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
+      <div className="container-fluid ">
+        <div className="col-2">
+          <a className="navbar-brand" href="#">
+            <img className="logo-navb" src={logo} />
+          </a>
+        </div>
+        <div className="menu-navbar col-10">
+          <div className="button-navbar ">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon bg-light "></span>
+            </button>
+          </div>
+          <div
+            className="listanav ul-nav collapse navbar-collapse"
+            id="navbarNavDropdown"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Inicio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Trabajos Realizados
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Presupuesto
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Otros
+                </a>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Contactenos
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Recomendaciones
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,15 +6,5 @@ import ReactPlayer from "react-player";
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
-  return (
-    <div className="text-center mt-5">
-      <ReactPlayer
-        url="https://www.youtube.com/watch?v=rnwlWn603g4"
-        className="react-player"
-        playing
-        width="100%"
-        height="100%"
-      />
-    </div>
-  );
+  return <div className="contenthome text-center mt-5"></div>;
 };
