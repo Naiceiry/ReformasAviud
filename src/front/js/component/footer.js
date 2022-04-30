aa4d0d7c4f3d8d82e4fcdfde9857e96e9e81ab34
Naiceiry <naiceirymp@gmail.com>
2022-04-30 16:19:10 +0000

    hacer presentacion de entrada para ya subir

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -6,16 +6,22 @@ import youtb from "../../img/youtb.png";
 import intg from "../../img/intg.png";
 export const Footer = () => (
   <footer className="footer mt-auto py-3 text-center">
-    <a className="navbar-brand" href="#">
+    <a className="navbar-brand" href="mailto:reformasaviud@gmail.com">
       <img className="img-footer" src={gmail} />
     </a>
-    <a className="navbar-brand" href="#">
+    <a
+      className="navbar-brand"
+      href="https://api.whatsapp.com/send?phone=+34651667144"
+    >
       <img className="img-footer" src={wsapp} />
     </a>
-    <a className="navbar-brand" href="#">
+    <a className="navbar-brand" href="https://goo.gl/maps/ZRNcayEQmWSgEidP9">
       <img className="img-footer" src={maps} />
     </a>
-    <a className="navbar-brand" href="#">
+    <a
+      className="navbar-brand"
+      href="https://www.youtube.com/channel/UChPQF4zFH-Oqf2A-b2cYKqA"
+    >
       <img className="img-footer" src={youtb} />
     </a>
     <a className="navbar-brand" href="#">
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,5 +6,19 @@ import ReactPlayer from "react-player";
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
-  return <div className="contenthome text-center mt-5"></div>;
+  return (
+    <div className="contenthome text-center mt-5">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe
+          className="VideoIntro"
+          width="300"
+          height="215"
+          src="https://www.youtube.com/embed/1Ud9KBbj2II?autoplay=1&mute=1&loop=1&controls=0"
+          title="Reformas Aviud Intro"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  );
 };
